a357073dffefbfa1e7f40da7699579bfa68fd9a5
Noel <noel.gabrielsson@gmail.com>
2024-09-26 10:49:16 +0200

    bilder

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
-});
+  }
